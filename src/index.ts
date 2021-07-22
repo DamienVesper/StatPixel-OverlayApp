04ea7197aab6b2874eb6aa003373a67df74e043b
Damien Vesper <ldamienvesper@gmail.com>
2021-07-22 17:43:38 -0500

    init electron

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import Electron, { app, BrowserWindow } from 'electron';
+
+let win: BrowserWindow;
+
+const createWindow = () => {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        titleBarStyle: 'hidden',
+        title: `Krew.io`,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile(`index.html`);
+    win.on(`closed`, () => win = null);
+}
+
+app.on(`ready`, () => {
+    createWindow();
+});
+
+app.on(`window-all-closed`, () => {
+    if(process.platform !== `darwin`) app.quit();
+    console.log(`Sucessfully shut down application.`); 
+});
+
+app.on(`activate`, () => {
+    if(win == null) createWindow();
+});
+
