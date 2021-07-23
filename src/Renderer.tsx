a900d4f36592839f87906087ae115ad809b67754
Damien Vesper <ldamienvesper@gmail.com>
2021-07-23 13:49:01 -0500

    react: init

--- src/Renderer.tsx
+++ src/Renderer.tsx
@@ -1,1 +1,9 @@
 import '../public/assets/css/index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './react/App';
+
+// Render the application.
+ReactDOM.render(<App />, document.querySelector(`#root`));
